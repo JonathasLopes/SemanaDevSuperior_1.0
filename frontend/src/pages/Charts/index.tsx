ac7a484bd9f51c16eb32c4adc8736e0e10dbdb76
Jonathas <jon.athasmoreira@hotmail.com>
2020-09-20 09:56:59 -0300

    Configurando front-web para produção

--- frontend/src/pages/Charts/index.tsx
+++ frontend/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-jonathas.herokuapp.com';
 
 const Charts = () => {
 
--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-jonathas.herokuapp.com';
 
 const Records = () => {
 
